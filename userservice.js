9a09a01cd808ba0b0c84d92046a7bdd536a1d0d9
Zumium <martin007323@gmail.com>
2016-05-12 22:43:37 +0800

    fix typo

--- userservice.js
+++ userservice.js
@@ -5,7 +5,7 @@ exports.vertifyUserLogin=function(id,password){
 	return new Promise((resolve,reject)=>{
 		db.StudentCache.findOne({id:id}).then((student)=>{
 			if(!student) return reject(new Error('Username or password is wrong'));
-			else if(student.password==password) return reject('Username or password is wrong');
+			else if(student.password!=password) return reject('Username or password is wrong');
 			else return resolve();
 		},(err)=>{
 			reject(err);
