8dfee9fa0c1de15e214e794da9e38e64b73d56a7
Zumium <martin007323@gmail.com>
2016-05-12 22:33:49 +0800

    add user service

---
+++ userservice.js
@@ -0,0 +1,14 @@
+var db=require('./db');
+var Promise=require('bluebird');
+
+exports.vertifyUserLogin=function(id,password){
+	return new Promise((resolve,reject)=>{
+		db.UserCache.findOne({id:id}).then((student)=>{
+			if(!student) return reject(new Error('Username or password is wrong'));
+			else if(student.password==password) return reject('Username or password is wrong');
+			else return resolve();
+		},(err)=>{
+			reject(err);
+		});
+	});
+}
