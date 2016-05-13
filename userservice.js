6ac90586c9a12d3aebe90d49cb9f545c3747dfde
Zumium <martin007323@gmail.com>
2016-05-13 15:41:28 +0800

    userservice stage 1 complete

--- userservice.js
+++ userservice.js
@@ -1,6 +1,7 @@
 var db=require('./db');
 var Promise=require('bluebird');
 
+//登录验证
 exports.vertifyUserLogin=function(id,password){
 	return new Promise((resolve,reject)=>{
 		db.StudentCache.findOne({id:id}).then((student)=>{
@@ -10,5 +11,35 @@ exports.vertifyUserLogin=function(id,password){
 		},(err)=>{
 			reject(err);
 		});
+	});
+}
+
+//返回同学是否是班委
+exports.isAdmin=function(id){
+	return new Promise((resolve,reject)=>{
+		db.StudentCache.findOne({id:id}).then((student)=>{
+			if(student.character=='同学') return resolve(false);
+			else return resolve(true);
+		},(err)=>{reject(err);});
+	});
+}
+
+//更换密码
+exports.changePassword=function(id,newPassword){
+	return new Promise((resolve,reject)=>{
+		db.StudentCache.findOne({id:id}).then((student)=>{
+			student.password=newPassword;
+			student.save();
+		},(err)=>{reject(err);});
+	});
+}
+
+//更改职位
+exports.changeCharacter=function(id,newCharacter){
+	return new Promise((resolve,reject)=>{
+		db.StudentCache.findOne({id:id}).then((student)=>{
+			student.character=newCharacter;
+			student.save();
+		},(err)=>{reject(err);});
 	});
 }
