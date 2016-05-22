86a947fec743a41045b61b68583bec010bed3030
Zumium <martin007323@gmail.com>
2016-05-22 16:21:39 +0800

    add new router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser=require('body-parser');
 var app=express();
 
 var student=require('./routers/student');
+var studentp=require('./routers/student-personal');
 //Initialize passport with given strategy
 passport.use(apiAuthStrategy);
 
@@ -14,5 +15,6 @@ app.use(passport.authenticate('basic',{session:false}));
 app.use(bodyParser.json());
 
 app.use('/users',student.router);
+app.use('/users/:sid',studentp.router);
 
 app.listen(8000);
---
+++ routers/student-personal.js
@@ -0,0 +1,13 @@
+var express=('express');
+var us=require('../userservice');
+
+var router=exports.router=express.Router();
+
+router.get('/',(req,res)=>{
+	var queriedUser=req.params.sid;
+	us.getStudentInfo(queriedUser).then((student)=>{
+		res.json(student.dataValues);
+	},(err)=>{
+		res.status(400).json({message:err.message});
+	});
+});
