3b0488ee683dee38b3a2907862f0ce5f49c0105b
Zumium <martin007323@gmail.com>
2016-05-23 23:19:53 +0800

    add post notification handler

--- routers/notification.js
+++ routers/notification.js
@@ -1,6 +1,8 @@
 var us=require('../userservice');
 var ns=require('../notiservice');
 var express=require('express');
+var Promise=require('bluebird');
+var ps=require('../pmsservice');
 
 var router=exports.router=express.Router();
 
@@ -13,6 +15,25 @@ router.get('/',(req,res)=>{
 		});
 		res.status(200).json(results);
 	},(err)=>{
+		res.status(500).json({message:err.message});
+	});
+});
+
+router.post('/',(req,res)=>{
+	var currentUser=req.user;
+	var data=req.body;
+	Promise.join(us.isAdmin(currentUser),ps.checkAttributes(['content','receivers'],null,data),(isAdmin,isGoodNoti)=>{
+		if(!isAdmin) return res.status(403).json({message:'Not permitted'});
+		if(!isGoodNoti) return res.status(400).json({message:'Something wrong within new notification information'});
+		if(!util.isArray(data.receivers)) return res.status(400).json({'Receivers should be an array'});
+		data=us.filtObject(['title','content','receivers'],data);
+		data['sender']=currentUser;
+		ns.publishNewNotification(data).then(()=>{
+			res.sendStatus(200);
+		},(err)=>{
+			res.status(400).json({message:err.message});
+		});
+	}).then(null,(err)=>{
 		res.status(500).json({message:err.message});
 	});
 });
