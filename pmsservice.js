9c12fe643d8be2188e758f6dfb44ed79653c1e7e
Zumium <martin007323@gmail.com>
2016-05-26 12:44:24 +0800

    check sender id

--- pmsservice.js
+++ pmsservice.js
@@ -1,5 +1,6 @@
 var Promise=require('bluebird');
 var util=require('util');
+var ns=require('./notiservice');
 
 exports.checkAttributes=function(includeAttrList,excludeAttrList,obj){
 	return new Promise((resolve,reject)=>{
@@ -23,5 +24,16 @@ exports.isOperateOnSelf=function(req,sid){
 		if(!req.user) return reject(new Error('No user logged'));
 		if(!sid) return reject(new Error('Empty \'sid\' argument'));
 		resolve(req.user==sid);
+	});
+}
+
+exports.isSender=function(nid,sid){
+	return new Promise((resolve,reject)=>{
+		ns.getNotificationById(nid).then((notification)=>{
+			notification.getSender().then((student)=>{
+				if(student.get('id')==sid) resolve(true);
+				else resolve(false);
+			},(err)=>{reject(err);});
+		},(err)=>{reject(err);});
 	});
 }
