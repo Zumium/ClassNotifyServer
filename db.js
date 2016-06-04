b7b1721d4f8a2325ba5f6fbbe31b5366e1ed9812
Zumium <martin007323@gmail.com>
2016-06-04 19:24:45 +0800

    bug fix

--- db.js
+++ db.js
@@ -56,7 +56,7 @@ var Notification=sequelize.define('notification',{
 	},
 	sender: {
 		type: Sequelize.STRING,
-		referencesKey: 'SenderID',
+		field: 'SenderID',
 		references: {
 			model: Student,
 			key: 'id'
@@ -105,8 +105,8 @@ var NotificationStatus=sequelize.define('notificationStatus',{
 Notification.belongsToMany(Student,{as:'Receivers',through:NotificationStatus,constraints:false});
 Student.belongsToMany(Notification,{as:'ReceivedNotifications',through:NotificationStatus,constraints:false});
 
-Notification.belongsTo(Student,{as:'Sender',constraints:false});
-Student.hasMany(Notification,{as:'SentNotifications',constraints:false});
+Notification.belongsTo(Student,{as:'Sender',foreignKey:'SenderID',constraints:false});
+Student.hasMany(Notification,{as:'SentNotifications',foreignKey:'SenderID',constraints:false});
 
 //exports all models
 exports.Student=Student;
