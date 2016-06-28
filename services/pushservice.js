f77ac9014d21300f00dff1f7ab0746c7cf967174
Zumium <martin007323@gmail.com>
2016-06-28 15:08:17 +0800

    restruction

--- services/pushservice.js
+++ services/pushservice.js
@@ -1,31 +1,47 @@
-var redis=require('redis');
 var io=require('socket.io');
+var ns=require('./userservice');
 
-const redisOptions={host:'redis'};
-var redisServer=redis.createClient(redisOptions);
 var pushServer=null;
-
-exports.pushNewNotification=function(id){
-	redisServer.publish('NewNotification',JSON.stringify({id:id}));
+var onlineClients={};
+//inner functions: manage socket.io clients
+function clientOnline(id,client){
+	onlineClients[id]=client;
+	client.userID=id;
 }
-
+function clientOffline(client){
+	del onlineClients[client.userID];
+}
+function clientEmit(ids,eventName,data){
+	ids.forEach((id)=>{
+		var client=onlineClients[id];
+		if(!client) return; //if client is offline,exit the function
+		client.emit(eventName,data);
+	});
+}
+//exported function: push a notification to its receivers
+exports.pushNewNotification=function(notification){
+	notification.getReceivers().then((receivers)=>{
+		var receiverIds=receivers.map((receiver)=>{return receiver.get('id');});
+		clientEmit(receiverIds,'newNotification',{id:notification.get('id')});
+	});
+}
+//exported function: initialize pushing service
 exports.init=function(server){
 	pushServer=io(server);
 	pushServer.on('connection',(client)=>{
-		//create a new redis client to represent the connecting user
-		//and subscribe the user to 'NewNotification' channel
-		var redisClient=redis.createClient(redisOptions);
-		redisClient.subscribe('NewNotification');
-	
-		//when new message has come
-		redisClient.on('message',(channelName,newNotiStr)=>{
-			client.emit('newNotification',JSON.parse(newNotiStr));
+
+		//Authorizing the new client
+		client.on('authorize',(id,password)=>{
+			us.vertifyUserLogin(id,password).then((isCorrect)=>{
+				if(!isCorrect) return; //authorization failed
+				clientOnline(id,client);
+			});
 		});
-	
-		//when user goes offline
-		pushServer.on('disconnect',()=>{
-			redisClient.unsubscribe();
-			redisClient.quit();
+
+		//close connection
+		client.on('disconnect',()=>{
+			clientOffline(client);
 		});
+
 	});
 }
