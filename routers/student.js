5a2fe117f970b6a3629c88f32ea17db81107508f
Zumium <martin007323@gmail.com>
2016-07-16 23:11:12 +0800

    seperate user notifications module

--- routers/student.js
+++ routers/student.js
@@ -1,44 +1,14 @@
 var express=require('express');
-var us=require('../services/userservice');
-var ns=require('../services/notiservice');
-var Promise=require('bluebird');
-var ps=require('../services/pmsservice');
-var genError=require('../tools/gene-error');
-var filtObject=require('../tools/filt-object');
 
 var studentPerson=require('./student-person');
 var studentSid=require('./student-sid');
+var studentNotification=require('./student-noti');
 
 var router=module.exports=express.Router();
 
 //====================================
-router.use('/',studentPerson);
+router.use('/',studentPerson); // url=/
 //====================================
-router.use('/',studentSid);
+router.use('/',studentSid);    // url=/:sid
 //=============================================
-//接下来是处理/users/:sid/notifications的中间件
-//=============================================
-router.get('/:sid/notifications',(req,res,next)=>{
-	var queriedUser=req.params.sid;
-	var currentUser=req.user;
-	//权限检查，不允许查看别人的通知
-	//if(queriedUser!=currentUser) return res.status(403).json({message:'Not allow to query others\' notifications'});
-	Promise.join(ps.isOperateOnSelf(req,queriedUser),(isSelf)=>{
-		//权限检查，不允许查看别人的通知
-		if(!isSelf) return next(genError(403,'Not permitted'));
-		ns.getPersonalNotifications(queriedUser,req.query).then((notifications)=>{
-			var results=[];
-			notifications.forEach((eachNoti)=>{
-				var t=eachNoti.get();
-				delete t['notificationStatus'];
-				results.push(t);
-			});
-			res.status(200).json(results);
-		},(err)=>{
-			next(err);
-		});
-	}).then(null,(err)=>{
-		next(err);
-	});
-});
-
+router.get('/',studentNotification); // url=/:sid/notifications
